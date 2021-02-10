443209d540d98edc9b71f64039871de1306137cd
cagatayy94 <33986736+cagatayy94@users.noreply.github.com>
2021-02-11 00:02:13 +0300

    Update remove_html_tags.php

--- remove_html_tags.php
+++ remove_html_tags.php
@@ -6,20 +6,13 @@
  * Time: 22:58
  */
 
-
 $subject = "<span style='color:red'> Julius Caesar</span>";
-
-
-
 $pattern = "/<\/?[^>]+>/";
 $replacement = "";
 
 $result = preg_replace($pattern, $replacement, $subject);
 
-
 echo "Subject : " .$subject;
 echo "<br>";
 echo "After Replace : ". $result;
-
 echo "<br><br><br> <b>IT ALLOWS TO REMOVE ALL HTML TAGS FROM THE SUBJECT</b>";
-
